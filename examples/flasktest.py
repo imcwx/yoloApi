d9ecb853e07756f335f1862ac40392bf8305a376
root <you@example.com>
2018-06-08 08:26:54 +0000

    update flask host and main

--- examples/flasktest.py
+++ examples/flasktest.py
@@ -6,4 +6,4 @@ app = Flask(__name__)
 def hello_world():
 	return 'hello world!'
 
-app.run(debug=True, port=8888)
+app.run(debug=True, host='0.0.0.0', port=8888)
--- main.py
+++ main.py
@@ -71,4 +71,4 @@ if __name__ == '__main__':
            "please wait until server has fully started"))
     yolo, all_classes = demo.setup()
     graph = tf.get_default_graph()
-    app.run(debug=True, port=8888)
+    app.run(debug=True, host='0.0.0.0',port=8888)
